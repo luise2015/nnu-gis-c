2153904d443c5eec32b31c3a7468adcc690c9dd5
luise2019 <641597542@qq.com>
2020-06-15 22:52:31 +0800

    test5

--- 2003.c
+++ 2003.c
@@ -115,7 +115,67 @@ void test3()
     printf("%d", index);
 }
 
+//4.利用递归法找出自然数1,2,n中任取r个数的所有组合
+const int R = 4;
+void combine(int start, int n, int r, int bIndex, int b[])
+{
+    if (r == 0)
+    {
+        for (int k = 0; k < R; k++)
+        {
+            printf("%d ", b[k]);
+        }
+        printf("\n");
+    }
+    else
+    {
+        for (int i = start; i <= n + 1 - r; i++)
+        {
+            b[bIndex] = i;
+            combine(i + 1, n, r - 1, bIndex + 1, b);
+        }
+    }
+}
+
+void test4()
+{
+    int b[4];
+    combine(1, 5, R, 0, b);
+    // 1 2 3 4 5
+    //step1 1 从2 3 4 5中选取2个数
+    //step1-1 2 从 3 4 5中选取1个数
+    //step1-2 3 从 4 5中选取一个数
+    //step1-3 4 从5中选取一个数
+    //step2 2 从3 4 5中选取2个数
+    //step3 3 从 4 5中选取两个数
+}
+
+//5.单向循环链表和顺序存储结构设计一个算法解决约瑟夫环问题
+//设有N个人围坐一圈。现从某个人开始报数，数到M的人出列，直到所有人都出列为止
+//试求他们的出列次序
+void joseph(int arr[], int n, int m)
+{
+    int size = n;
+    int index = 0;
+    while (size > 0)
+    {
+        index = (index + m - 1) % size;
+        printf("%d\n", arr[index]);
+        for (int i = index; i < size - 1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        size--;
+    }
+}
+
+void test5()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    joseph(arr, 7, 3);
+}
+
 void main()
 {
-    test3();
+    test5();
 }
